07b7dad060a8172522f05f386d48239d6579bd6f
fahim27 <fahimmbpi@gmail.com>
2020-05-13 19:44:30 +0600

    fahim

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+//get the even/add number using if else statement
+
+let number=prompt('Enter number')
+
+if(number%2==0){
+
+console.log('the number is even ')
+}
+else{
+
+console.log('the number is odd')
+}
---
+++ switch.js
@@ -0,0 +1,50 @@
+//Digit spelling betwwen (0-0) chek usiung swirch controoll statement;
+let number=prompt('Enter your number:');
+
+switch(number){
+case '1':
+console.log('the number is one')
+break
+
+case '2':
+console.log('the number is two')
+break
+
+case '3':
+console.log('the number is three')
+break
+
+
+case '4':
+console.log('the number is four')
+break
+
+
+case '5':
+console.log('the number is five')
+break
+
+case '6':
+console.log('the number is six')
+break
+
+case '7':
+console.log('the number is seven')
+break
+
+case '8':
+console.log('the number is 8')
+break
+
+case '9':
+console.log('the number is nine')
+break
+
+case '0':
+console.log('the number is zero')
+break
+
+default:
+console.log('Invalid Number')
+break
+}
